122e95db3174ff19ac1129b4bfc314e172f92a1b
Huzaifa0012 <168412580+Huzaifa0012@users.noreply.github.com>
2024-05-08 13:57:44 +0500

    Add files via upload

---
+++ 04 Famous1.ts
@@ -0,0 +1,14 @@
+// let personName :string = '';
+
+// personName = prompt('What Is Your Name') || '';
+// let titlecase :string = personName.split(' ').map(word => word.charAt(0).toUpperCase()
+//+ word.slice(1).toLowerCase()).join('');
+// if(personName !== null && personName !== ''){
+//     alert(`Hello ${personName},
+//     titlecase: ${titlecase}
+//     `);
+// }
+// else{
+//     alert('Enter Name Please')
+// }
+console.log('Imran Khan Once Said, "Absolutly Not"');
